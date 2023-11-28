3c118c1958f785f05ae3faaf6d80a687e5c929db
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-11-27 20:57:20 -0700

    test and feat: test and complete the last 2 endpoints for the take home challenge

--- app/controllers/api/v1/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customer_subscriptions_controller.rb
@@ -1,15 +1,20 @@
 class Api::V1::CustomerSubscriptionsController < ApplicationController 
   def create
-    render json: CustomerSubscription.create!(subscription_params)
+    render json: CustomerSubscriptionSerializer.new(CustomerSubscription.create!(subscription_params))
   end
 
   def update
-    binding.pry
+    @customer_sub = CustomerSubscription.find(params[:id])
 
+    if @customer_sub.update(subscription_params)
+      render json: CustomerSubscriptionSerializer.new(@customer_sub)
+    end
   end
   
   def index
-    render json: CustomerSubscription.all
+    customer = Customer.find(params[:customer_id])
+    subscriptions = customer.customer_subscriptions
+    render json: CustomerSubscriptionSerializer.new(subscriptions)
   end
 
   private
